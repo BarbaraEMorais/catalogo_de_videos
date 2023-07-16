6ce0636fda7d40cfce28149471eee0168f50ab13
Gabriel Breder <gabrielbertobreder@gmail.com>
2023-07-16 14:32:13 -0300

    feat: updating routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:catalogo_de_videos/pages/home.dart';
 import 'package:flutter/material.dart';
 
 import 'pages/login.dart';
 
 final routes = {
-  '/': (BuildContext context) => const LoginPage(),
+  '/': (BuildContext context) => const HomePage(),
   '/login': (BuildContext context) => const LoginPage()
 };
 
