84f73bff96bd677bc265c0f74148ae42db8db771
Gabriel Breder <gabrielbertobreder@gmail.com>
2023-07-17 16:18:11 -0300

    fix: turning VideoCard immutable

--- lib/components/video_card.dart
+++ lib/components/video_card.dart
@@ -2,20 +2,20 @@ import 'package:flutter/material.dart';
 import '../styles/theme_colors.dart';
 
 class VideoCard extends StatelessWidget {
-  late String url;
-  late String name;
-  late double cardWidth;
-  VideoCard({required this.name, required this.url, super.key});
+  final String url;
+  final String name;
+  const VideoCard({required this.name, required this.url, super.key});
 
   @override
   Widget build(BuildContext context) {
+    double cardWidth;
     double screenWidth = MediaQuery.of(context).size.width;
     cardWidth = (screenWidth - 50) / 2.5;
 
     return Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10),
         child: GestureDetector(
-            onTap: () => {print(name)},
+            onTap: () => {},
             child: Column(children: [
               Container(
                   margin: const EdgeInsets.symmetric(vertical: 5),
