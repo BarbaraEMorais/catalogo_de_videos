ccf0592e6b2c5999c2aea0ddd5d7670dd4b11e1e
Gabriel Breder <gabrielbertobreder@gmail.com>
2023-07-16 22:42:49 -0300

    feat: updating theme

--- lib/styles/theme_colors.dart
+++ lib/styles/theme_colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 class ThemeColors {
-  static Color background = const Color.fromARGB(17, 17, 17, 1);
-  static Color text = const Color.fromARGB(255, 250, 248, 248);
+  static Color background = const Color.fromARGB(17, 17, 17, 255);
+  static Color text = const Color.fromARGB(255, 255, 255, 255);
 }
